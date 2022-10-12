626065e690c2e4645dd3d0e01f41737f83b353a0
Matteo Artuso <10610150@polimi.it>
2022-10-12 17:48:44 +0200

    error handler

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import logging
-from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
+import html
+import json
+import traceback
+from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove, ParseMode
 from telegram.ext import Updater, CommandHandler, CallbackContext, ConversationHandler, MessageHandler, Filters
 import Token
 from os import listdir
@@ -45,7 +48,23 @@ def error_handler(update: Update, context: CallbackContext):
     """Log the error and send a telegram message"""
     # Log the error before we do anything else, so we can see it even if something breaks.
     logger.error(msg="Exception while handling an update:", exc_info=context.error)
-    update.message.reply_text("errore")
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = ''.join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f'An exception was raised while handling an update\n'
+        f'<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}'
+        '</pre>\n\n'
+        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
+        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
+        f'<pre>{html.escape(tb_string)}</pre>'
+    )
+
+    # Finally, send the message
+    context.bot.send_message(chat_id=chat_id, text=message, parse_mode=ParseMode.HTML)
 
 
 def start(update: Update, context: CallbackContext):
@@ -93,7 +112,7 @@ def scap(update: Update, context: CallbackContext):
             update.message.reply_text("SCAP COIN FINITI, se ne vuoi altri https://www.paypal.me/matteoartuso99")
             return
         if SCAP[user.name] == -10:
-            update.message.reply_text("CONGRATULAZIONI sei un coglione, hai usato /scap 10 volte in più del limite")
+            update.message.reply_text("CONGRATULAZIONI sei un COGLIONE, hai usato /scap 10 volte in più del limite")
             return
         if SCAP[user.name] < -1:
             return
