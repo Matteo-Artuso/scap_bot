adab8b0457d55a0468552e5fb9e357bd7c237da0
Matteo Artuso <10610150@polimi.it>
2022-10-14 15:46:18 +0200

    error handler

--- main.py
+++ main.py
@@ -45,26 +45,7 @@ def reset_scap_coin(context: CallbackContext):
 
 # BOT HANDLERS FUNCTIONS
 def error_handler(update: Update, context: CallbackContext):
-    """Log the error and send a telegram message"""
-    # Log the error before we do anything else, so we can see it even if something breaks.
-    logger.error(msg="Exception while handling an update:", exc_info=context.error)
-    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
-    tb_string = ''.join(tb_list)
-
-    # Build the message with some markup and additional information about what happened.
-    # You might need to add some logic to deal with messages longer than the 4096 character limit.
-    update_str = update.to_dict() if isinstance(update, Update) else str(update)
-    message = (
-        f'An exception was raised while handling an update\n'
-        f'<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}'
-        '</pre>\n\n'
-        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
-        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
-        f'<pre>{html.escape(tb_string)}</pre>'
-    )
-
-    # Finally, send the message
-    context.bot.send_message(chat_id=chat_id, text=message, parse_mode=ParseMode.HTML)
+    update.message.reply_text("errore")
     return ConversationHandler.END
 
 
