62a7baa33d7781572aa4b097234854cbfd420ece
Matteo Artuso <10610150@polimi.it>
2023-10-19 13:02:56 +0200

    error handler

--- tests/main.py
+++ tests/main.py
@@ -10,7 +10,7 @@ TOKEN = pathlib.Path("test_token.txt").read_text().strip()
 
 # ## START ## #
 if __name__ == '__main__':
-    scap_bot = ScapBot(TEST_CHAT_ID, DAILY_COINS, TOKEN)
+    scap_bot = ScapBot(TEST_CHAT_ID, TEST_CHAT_ID, DAILY_COINS, TOKEN)
     application = scap_bot.get_updater()
 
     # Start the Bot
